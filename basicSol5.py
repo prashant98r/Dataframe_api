b5bde01f397a5d778d736b41782bf9a9243a7c29
kprashant <prashant98r@gmail.com>
2024-09-21 22:31:16 +0530

    Added .toDF method to name the columns

--- basicSol5.py
+++ basicSol5.py
@@ -5,6 +5,8 @@ from pyspark.sql.functions import col, when
 
 # Set the path to the Python interpreter
 os.environ["PYSPARK_PYTHON"] = "C:/Users/kprat/Documents/Python/Python/Python37/python.exe"
+os.environ["JAVA_HOME"] ="C:/Program Files/Java/jdk1.8.0_202"
+os.environ["SPARK_HOME"]="C:/Users/kprat/Documents/spark-3.3.1-bin-hadoop3/spark-3.3.1-bin-hadoop3"
 
 # Create a SparkSession
 spark = (SparkSession.builder
@@ -14,9 +16,9 @@ spark = (SparkSession.builder
          .getOrCreate())
 spark.sparkContext.setLogLevel("ERROR")
 events=[(1, "2024-07-27"),(2, "2024-12-25"),(3, "2025-01-01")]
-columns=["event_id","date"]
+#columns=["event_id","date"]
 
-events_df= spark.createDataFrame(events,columns)
+events_df= spark.createDataFrame(events).toDF("event_id","date")
 events_with_is_holiday= events_df.withColumn("is_holiday",
                                                        when((col("date")=="2024-12-25" ) | (col("date")=="2025-01-01"),True)
                                                        .otherwise(False))
