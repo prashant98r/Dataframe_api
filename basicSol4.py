d15f76eb742d265f991b40c09464e4a5d405a570
kprashant <prashant98r@gmail.com>
2024-09-21 22:32:14 +0530

    Added .toDF method to name the columns

--- basicSol4.py
+++ basicSol4.py
@@ -5,6 +5,8 @@ from pyspark.sql.functions import col, when
 
 # Set the path to the Python interpreter
 os.environ["PYSPARK_PYTHON"] = "C:/Users/kprat/Documents/Python/Python/Python37/python.exe"
+os.environ["JAVA_HOME"] ="C:/Program Files/Java/jdk1.8.0_202"
+os.environ["SPARK_HOME"]="C:/Users/kprat/Documents/spark-3.3.1-bin-hadoop3/spark-3.3.1-bin-hadoop3"
 
 # Create a SparkSession
 spark = (SparkSession.builder
@@ -14,9 +16,8 @@ spark = (SparkSession.builder
          .getOrCreate())
 spark.sparkContext.setLogLevel("ERROR")
 products=[(1, 30.5),(2, 150.75),(3, 75.25)]
-columns=["product_id","price"]
 
-products_df= spark.createDataFrame(products,columns)
+products_df= spark.createDataFrame(products).toDF("product_id","price")
 products_with_price_range= products_df.withColumn("price_range",
                                                        when(col("price") < 50,"Cheap")
                                                        .when((col("price")>=50) & (col("price")<=100),"Moderate")
