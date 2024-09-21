84295fe663c51b18deb891035d519759d965b109
kprashant <prashant98r@gmail.com>
2024-09-21 22:33:48 +0530

    Fixed errors by adding java and spark environ

--- df2.py
+++ df2.py
@@ -2,13 +2,19 @@
 from pyspark.sql import SparkSession
 import os
 
-#from pyspark.sql.functions import date_add
-
 # Set the path to the Python interpreter
 os.environ["PYSPARK_PYTHON"] = "C:/Users/kprat/Documents/Python/Python/Python37/python.exe"
+os.environ["JAVA_HOME"] ="C:/Program Files/Java/jdk1.8.0_202"
+os.environ["SPARK_HOME"]="C:/Users/kprat/Documents/spark-3.3.1-bin-hadoop3/spark-3.3.1-bin-hadoop3"
 
 # Create a SparkSession
-spark = SparkSession.builder.getOrCreate()
+spark = (SparkSession.builder
+         .config("spark.ui.port", "4040")
+         .appName("spark-program")
+         .master("local[*]")
+         .getOrCreate())
+spark.sparkContext.setLogLevel("ERROR")
+
 
 df = spark.read \
 .format("csv") \
